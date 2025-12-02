6236b38e4a474f801601c8451a789dafb633b551
bassant hossam <125453475+Bassanthossamxx@users.noreply.github.com>
2025-12-03 01:39:09 +0200

    try to solve logo

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -13,6 +13,8 @@ import {
 } from "lucide-react";
 import { useTranslation } from "react-i18next";
 import type { TabType } from "../../dashboard/types";
+import logoDark from "../../assets/images/logo_dark.png";
+import logoLight from "../../assets/images/logo_light.png";
 
 interface SidebarProps {
   activeTab: TabType;
@@ -58,11 +60,7 @@ export function Sidebar({
             className={`flex items-center ${!sidebarOpen && "justify-center"}`}
           >
             <img
-              src={
-                darkMode
-                  ? "/src/assets/images/logo_dark.png"
-                  : "/src/assets/images/logo_light.png"
-              }
+              src={darkMode ? logoDark : logoLight}
               alt="Kalamna Logo"
               className="w-10 h-10 object-contain transition-all"
             />
