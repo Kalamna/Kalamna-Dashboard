04874172fd2f275a3d370620c6bbb3070b882e47
Shahda <shahdamohamedabotalb@gmail.com>
2025-12-28 18:01:14 +0200

    fix card layout

--- src/components/overview/OverviewSection.tsx
+++ src/components/overview/OverviewSection.tsx
@@ -162,7 +162,7 @@ export function OverviewSection() {
       </div>
 
       {/* Overview Cards */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
         {cards.map((card, index) => (
           <div
             key={index}
